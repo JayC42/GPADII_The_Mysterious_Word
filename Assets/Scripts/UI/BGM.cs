4f6d97f342cba21d638ae0ffa76889bda2f91282
Chunzx14875 <czx14875@1utar.my>
2022-04-21 14:24:49 +0800

    Add BGM controller

---
+++ Assets/Scripts/UI/BGM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    private AudioSource bgm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgm.volume = BGMController.volume;
+    }
+}
---
+++ Assets/Scripts/UI/BGMController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour
+{
+    public static BGMController instance;
+    private AudioSource audiosource;
+
+    public static float volume = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        audiosource.volume = volume;
+
+    }
+
+    public void SetVolume(float vol)
+    {
+        volume = vol;
+    }
+
+    public void ToggleMusic()
+    {
+        audiosource.mute = !audiosource.mute;
+    }
+}
